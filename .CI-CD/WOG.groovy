85988e5dc70643078ce3e519c65dc9e6275d52e7
Moses, Naor <naor.moses@gmail.com>
2024-12-09 23:37:26 +0200

    configured CICD pipeline

--- .CI-CD/WOG.groovy
+++ .CI-CD/WOG.groovy
@@ -2,12 +2,47 @@ pipeline {
     agent any
     
     stages {
-        stage('test') {
+        stage('build') {
             steps {
                 script {
-                    sh("docker ps")
+                    sh("docker build -t naorj/wog-flask-apps:${env.BUILD_NUMBER} .")
                 }
             }
         }
+
+        stage('scan image') {
+            steps {
+                script {
+                    sh("docker images")
+                }
+            }
+        }
+
+        stage('test') {
+            steps {
+                script {
+                    sh("docker run -d -p 5000:5000 wog-flask-app:${env.BUILD_NUMBER}")
+                    sh("pip3 install selenium && python3 e2e.py http://host.docker.internal:5000/score")
+                }
+            }
+        }
+
+        stage('publish') {
+            steps {
+                script {
+                    withCredentials([usernamePassword(credentialsId: 'DockerHubCreds', passwordVariable: 'pass', usernameVariable: 'user')]) {
+                        sh("docker login -u ${user} -p ${pass}")
+                        sh("docker push naorj/wog-flask-app:${env.BUILD_NUMBER}")
+                    }
+                }
+            }   
+        }
     }
+
+    // post {
+    //     always {}
+    //     success{}
+    //     failure{}
+    //     unstable{}
+    // }
 }
